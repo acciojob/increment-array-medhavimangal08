184f17e580a0547ae6a5886dc83d40b7fc29a1a7
Medhavimangal43 <medhavimangal43@gmail.com>
2024-10-01 20:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	let new=[]
+	for(let a of arr){
+		new.push(++a)
+	}
+	return new
+	
 }
 
 alert(incrementArray(arr));
